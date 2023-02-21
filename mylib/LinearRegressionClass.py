2d6021f4f8907d7e510d35b037cf7e26dd0257b5
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2023-02-22 02:24:07 +0600

    без понятия

--- mylib/LinearRegressionClass.py
+++ mylib/LinearRegressionClass.py
@@ -1,35 +1,63 @@
 import numpy as np
-from scipy import optimize as opt
+from scipy.optimize import minimize
 
 
-class My_LinearRegression:
-    coef: np.array
-    m: int
+class MyLinearRegression_L2:
     X_train: np.array
     y_train: np.array
+    coef: np.array
+    alpha: int
+    l: int
+    d: int
+    eps: float
 
-    def __init__(self, X_train, y_train):
-        self.X_train = np.array(X_train)
+
+    def __init__(self, X_train, y_train, alpha, eps):
+
+        self.X_train = np.c_[np.ones(X_train.shape[0]), X_train]
         self.y_train = np.array(y_train)
-        self.m = X_train.shape[1]
-        self.coef = np.zeros(self.m)
+        self.l = X_train.shape[0]
+        self.d = X_train.shape[1] + 1
+        self.coef = np.zeros(self.d)
+        self.alpha = alpha
+        self.eps = eps
+
 
     def fit(self):
-        '''
-        Функция, подбирающая значения коэффициентов
-        '''
-        self.coef = opt.least_squares(self.loss_function, self.coef).x
-        return self
+        self.optimizator()
+
 
     def predict(self, X_test):
-        '''
-        Функция по вычислению регрессии (выдача предсказания, исходя из коэффициентов)
-        '''
-        return np.matmul(X_test, self.coef)
+        return np.dot(np.c_[np.ones(X_test.shape[0]), X_test], self.coef)
+        
     
-    def loss_function(self, c: np.array):
-        '''
-        Функция потерь
-        '''
-        return np.sum((np.array(self.y_train) - np.matmul(self.X_train, c)) ** 2)
+    def loss_function(self, w):
+        return (np.sum((self.y_train - np.dot(self.X_train, w)) ** 2) + self.alpha * np.sum(w ** 2)) / self.l
     
+
+    def optimizator(self, k=0.00001):
+        w0 = self.coef
+        t = 1
+        w1 = w0 - (k / t) * self.nabla(w0)
+        print(f'{t}) {round(self.loss_function(w0), 1)}')
+        while t <= 100:
+            t += 1
+            print(f'{t}) {round(self.loss_function(w1), 1)}')
+            w0, w1 = w1, w1 - (k / t) * self.nabla(w1)
+        self.coef = w1
+        # self.coef = minimize(self.loss_function, self.coef, method='SLSQP').x
+
+
+    def nabla(self, w):
+        grad = []
+        for i in range(self.d):
+            a = 0
+            for j in range(self.l):
+                a += ((self.y_train[j] - np.dot(w, self.X_train[j]))* self.X_train[j][i])
+            grad.append((-2 / self.l) * (a - self.alpha * w[i]))
+        return np.array(grad)
+
+
+
+
+
