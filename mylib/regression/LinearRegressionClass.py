b70e39563d05a0b7e6d83ea2dc2e19c6bd488407
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2023-06-07 09:30:18 +0600

    lab7

--- mylib/regression/LinearRegressionClass.py
+++ mylib/regression/LinearRegressionClass.py
@@ -3,35 +3,27 @@ from scipy.optimize import minimize
 
 
 class MyLinearRegression_L2:
-    X_train: np.array
-    y_train: np.array
-    coef: np.array
-    alpha: int
-    l: int
-    d: int
-    eps: float
+    def __init__(self, alpha: float, eps: float) -> None:
+        self.alpha = alpha
+        self.eps = eps
 
 
-    def __init__(self, X_train, y_train, alpha, eps):
-
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
         self.X_train = np.c_[np.ones(X_train.shape[0]), X_train]
         self.y_train = np.array(y_train)
         self.l = X_train.shape[0]
         self.d = X_train.shape[1] + 1
         self.coef = np.zeros(self.d)
-        self.alpha = alpha
-        self.eps = eps
-
-
-    def fit(self):
         self.optimizator()
 
+        return self
 
-    def predict(self, X_test):
+
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
         return np.dot(np.c_[np.ones(X_test.shape[0]), X_test], self.coef)
     
 
-    def optimizator(self, k=0.00001):
+    def optimizator(self, k=0.00001) -> None:
         w0 = self.coef
         t = 1
         w1 = w0 - (k / t) * self.nabla(w0)
@@ -42,7 +34,7 @@ class MyLinearRegression_L2:
         # self.coef = minimize(self.loss_function, self.coef, method='SLSQP').x
 
 
-    def nabla(self, w):
+    def nabla(self, w) -> np.ndarray:
         grad = []
         for i in range(self.d):
             a = 0
