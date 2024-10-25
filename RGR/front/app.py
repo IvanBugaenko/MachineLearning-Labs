fa6d1a6a8f430cfb3040270e9db77a33460e9e45
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-10-26 01:01:46 +0600

    com

--- RGR/front/app.py
+++ RGR/front/app.py
@@ -1,11 +1,34 @@
+# import streamlit as st
+# from streamlit_option_menu import option_menu
+# from IoC import IoC
+
+
+# with st.sidebar:
+#     selected = option_menu("Меню", ["Описание данных", 'Визуализации', 'Предсказание'], 
+#         icons=['info-circle', 'bar-chart', 'tag'], menu_icon="cast", default_index=0)
+
+
+# IoC["app"][selected]()
+
 import streamlit as st
-from streamlit_option_menu import option_menu
-from IoC import IoC
+from st_draggable_list import DraggableList
 
+st.title("DraggableList")
 
-with st.sidebar:
-    selected = option_menu("Меню", ["Описание данных", 'Визуализации', 'Предсказание'], 
-        icons=['info-circle', 'bar-chart', 'tag'], menu_icon="cast", default_index=0)
+data = [
+    {"id": "oct", "order": 10, "name": "Oct"},
+    {"id": "nov", "order": 11, "name": "Nov"},
+    {"id": "dec", "order": 12, "name": "Dec"},
+    {"id": "jan", "order": 1, "name": "Jan"},
+    {"id": "feb", "order": 2, "name": "Feb"},
+    {"id": "mar", "order": 3, "name": "Mar"},
+    {"id": "apr", "order": 4, "name": "Apr"},
+    {"id": "may", "order": 5, "name": "May"},
+    {"id": "jun", "order": 6, "name": "Jun"},
+    {"id": "jul", "order": 7, "name": "Jul"},
+    {"id": "aug", "order": 8, "name": "Aug"},
+    {"id": "Sep", "order": 9, "name": "Sep"},
+]
 
-
-IoC["app"][selected]()
+slist = DraggableList(data, width="100%")
+st.write(slist)
--- RGR/front/functions/prediction/models.py
+++ RGR/front/functions/prediction/models.py
@@ -3,7 +3,7 @@ import joblib
 
 pipe_knn = joblib.load('..\preprocessing\pipe_knn.pkl')
 pipe_bagging = joblib.load('..\preprocessing\pipe_bagging.pkl')
-pipe_dence = joblib.load('..\preprocessing\pipe_dence.pkl')
+pipe_dence = joblib.load('..\preprocessing\pipe_dence.h5')
 
 
 models = {
